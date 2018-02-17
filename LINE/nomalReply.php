962c3fe3cbb12786b1160db680e2ca5e823def60
howjoygroup <36475227+howjoygroup@users.noreply.github.com>
2018-02-17 12:22:45 +0800

    Update nomalReply.php

--- LINE/nomalReply.php
+++ LINE/nomalReply.php
@@ -69,13 +69,14 @@ function KeyWordReply($inputStr) {
 		stristr($Answer, '打') != false||
 		stristr($Answer, '誰') != false||
 		stristr($Answer, '秘書') != false) {
-			$rplyArr = Array(
-                 '人生是掌握在自己手裡的',
-                 '每個都很好哦',
-                 '不要把這麼重要的事情交給我決定比較好吧');
-		$Answer = $rplyArr[Dice(count($rplyArr))-1];
+			if(stristr($Answer, ' ') != false||
+				$rplyArr = Array(
+                 		'人生是掌握在自己手裡的',
+                 		'每個都很好哦',
+                 '		不要把這麼重要的事情交給我決定比較好吧');
+				$Answer = $rplyArr[Dice(count($rplyArr))-1];
 		}
-    return buildTextMessage('我想想喔……我覺得'.$Answer.'。');
+    	return buildTextMessage('我想想喔……我覺得'.$Answer.'。');
 	}
 	else    
 	//以下是運勢功能
