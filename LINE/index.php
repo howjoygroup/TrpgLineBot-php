d182b3e4e03724805c2c9b0cdbf84646d902846a
CHEN,Hung-Hao <36475227+howjoygroup@users.noreply.github.com>
2018-03-26 18:52:06 +0800

    Update index.php

--- LINE/index.php
+++ LINE/index.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Copyright 2016 LINE Corporation
  *
@@ -16,25 +17,19 @@
  */
 
 require_once('./LINEBotTiny.php');
+
 require_once('./nomalReply.php');
 require_once('./Dice/Dice_CoC7th.php');
 require_once('./Dice/Dice_nomalDice.php');
 require_once('./Dice/Dice_pbta.php');
-require_once('./Dice/Dice_extraDice.php');
-require_once('./Dice/Dice_test.php');
+
 
 //主要的全域變數，只有簡易的API，覺得難過香菇
-//試著手動加入了getProfile的功能…不知道是否用得到
 $channelAccessToken = getenv('LINE_CHANNEL_ACCESSTOKEN');
 $channelSecret = getenv('LINE_CHANNEL_SECRET');
-$keyWord = getenv('KEY_WORD');
-$manualUrl = getenv('MANUAL_URL');
-$textReplyUrl = getenv('TEXT_REPLY_URL');
-$imgsReplyUrl = getenv('IMGS_REPLY_URL');
-$yababangUrl = getenv('YABABANG_URL');
 
 $bot = new LINEBotTiny($channelAccessToken, $channelSecret);
-$userName = '你';
+
 
 
 //建立文字訊息的函數
@@ -81,7 +76,7 @@ function buildStickerMessage($packageId, $stickerId){
 }
 
 
-//建立複數訊息，的物件
+//建立複數訊息的物件
 class MutiMessage{
 
 	public function send($inputArr){	
@@ -125,20 +120,6 @@ class MutiMessage{
 	return $message;
 	}
 	
-	//建立旋轉木馬訊息(???)的函數
-	public function carousel($altText, $columns){	
-		error_log("準備回傳旋轉木馬訊息（殺小啦wwww");
-		$message = array(
-			'type'=> "template",
-			'altText'=> $altText,
-			'template'=> array(
-				'type'=> "carousel",
-				'columns'=> $columns
-            )
-		);
-	return $message;
-	}
-	
 }
 
 
@@ -149,39 +130,18 @@ foreach ($bot->parseEvents() as $event) {
     switch ($event['type']) {
 		//收到訊息的動作
         case 'message':
-			$message = $event['message'];			
-			$source = $event['source'];
-			if($source['type'] == "group"){		
-				
-				$groupId = $source['groupId'];
-				$userId = $source['userId'];
-				error_log("群組ID：".$groupId);
-				if($userId != null){
-								
-					$userName = $bot->getGroupProfile($groupId,$userId)['displayName'];
-					error_log("訊息發送人：".$userName);
-					}
-				else{
-					error_log("訊息發送人：不明");
-				}
-				}
-			if($source['type'] == "user"){
-				$userName = $bot->getProfile($source['userId'])['displayName'];
-				error_log("訊息發送人：".$userName);
-				}
-			
+			$message = $event['message'];
 			
 			//對訊息類別做篩選
-            switch ($message['type']) {				
+            switch ($message['type']) {
 				
 				//只針對文字訊息去回應
                 case 'text':
                 	$m_message = $message['text'];
-					
                 	if($m_message!="")
                 	{
-											
 						error_log("收到訊息：".$m_message);
+						error_log("replyToken：".$event['replyToken']);
 						$messages = parseInput($m_message);
 						
 						if ($messages == null) {
@@ -194,22 +154,13 @@ foreach ($bot->parseEvents() as $event) {
 							'replyToken' => $event['replyToken'],
 							'messages' => $messages
 							)
-						);	
+						);
+						
+						
 
                 	}
                     break;
                 
-				case 'image':
-				error_log("傳送了圖片。");
-				break;
-				
-				case 'video':
-				error_log("傳送了影片。");
-				break;
-				
-				case 'sticker':
-				error_log("傳送了貼圖。");
-				break;
             }
             break;
 			
@@ -218,7 +169,7 @@ foreach ($bot->parseEvents() as $event) {
 			error_log("被加入聊天室");
 			$messages = new MutiMessage();
 			$replyArr = Array(
-				$messages->text("大家好，我是宏豪的".$keyWord."。\n請輸入「".$keyWord."說明」獲得使用說明。"),
+				$messages->text("大家好，我是擲骰機器狗。\n請輸入「骰子狗說明」獲得使用說明～"),
 				$messages->sticker(1,2)
 			);
 			
@@ -230,12 +181,12 @@ foreach ($bot->parseEvents() as $event) {
 			);		
 			break;
 			
-			//被加入好友的動作
+		//被加入好友的動作
 		case 'follow':
 			error_log("被加入好友");
 			$messages = new MutiMessage();
 			$replyArr = Array(
-				$messages->text("你好哦，我是宏豪的".$keyWord."。\n請輸入「".$keyWord."說明」獲得使用說明。"),
+				$messages->text("你好哦，我是擲骰機器狗。\n請輸入「骰子狗說明」獲得使用說明。"),
 				$messages->sticker(4,631),
 				$messages->text("建議使用手機界面，可以更簡單的選取說明哦。")
 			);
@@ -256,20 +207,12 @@ foreach ($bot->parseEvents() as $event) {
 
 //這是基本判斷式
 function parseInput ($inputStr){
-	global $userName;
-	global $keyWord;
-	global $manualUrl;
-	global $textReplyUrl;
-	global $imgsReplyUrl;
-	global $yababangUrl;
-	
-
-	//error_log("訊息【".$inputStr."】進入parseInput");
-	$inputStr = strtolower($inputStr);
+	$replyKeyword = '骰子狗';
+	error_log("訊息【".$inputStr."】進入parseInput");
 
 	//preg_match ( "/A/" , B)。A是要比對的關鍵字（正則），B是被比對的字串
 	if (preg_match ("/dvtest/i", $inputStr)){
-		return DvTest ($inputStr,$userName,$textReplyUrl,$imgsReplyUrl);
+		return DvTest ($inputStr);
 		
 	}else if (preg_match ("/^cc/i", $inputStr)){
 		return CoC7th($inputStr);
@@ -277,17 +220,17 @@ function parseInput ($inputStr){
 	}else if(preg_match ("/^pb/i", $inputStr)){		
 		return pbta($inputStr);
 		
-	}else if(stristr($inputStr,$keyWord) != false){ //$keyWord
-		return KeyWordReply($inputStr,$keyWord,$manualUrl,$textReplyUrl,$userName);
-				
-	}else if(stristr($inputStr,".jpg") != false || stristr($inputStr,"") != false){
-		return SendImg($inputStr,$imgsReplyUrl);
+	}else if(stristr($inputStr,$replyKeyword) != false){
+		return KeyWordReply($inputStr);	
+
+	}else if(stristr(strtolower($inputStr),".jpg") != false||stristr(strtolower($inputStr),"ry") != false){
+		return SendImg($inputStr);
+		
+	}else if(stristr($inputStr,"mobile") != false){
+		return mobile($inputStr);			
 		
 	}else if(preg_match ("/d/i", $inputStr) !=false){
 		return nomalDiceRoller($inputStr);
-		
-	}else if(preg_match ("/b/i", $inputStr) !=false){
-		return bDice($inputStr);
 	}
 	
 	
@@ -297,11 +240,8 @@ function parseInput ($inputStr){
 }
 
 
-function DvTest ($inputStr,$userName,$textReplyUrl,$imgsReplyUrl){
-
-
+function DvTest ($inputStr){
 	error_log("進入DvTest");
-	
 	if(preg_match ("/muti|複數|多重/i", $inputStr) !=false){
 	$testMessage = new MutiMessage();
 	$replyArr = Array(
@@ -313,91 +253,19 @@ function DvTest ($inputStr,$userName,$textReplyUrl,$imgsReplyUrl){
 	
 	return $testMessage->send($replyArr);
 	}
+	/*
+	if(preg_match ("/muti|複數|多重/i", $inputStr) !=false){
+	$testMessage = new MutiMessage();
+	$replyArr = Array(
+		$testMessage->text('多重訊息演示'),
+		$testMessage->text('test2'),
+		$testMessage->img('https://i.imgur.com/k4QE5Py.png'),
+		$testMessage->sticker(1,2)
+		);
 	
-	if(preg_match ("/key|關鍵/i", $inputStr) !=false){
+	return $testMessage->send($replyArr);
+	}*/
 	
-	//抓文字關鍵字
-	$reply = "《文字關鍵字列表》\n";
-	//讀入文字回應變數
-	$content = file_get_contents($textReplyUrl);
-	
-	//如果失敗就調用預設值
-	if ($content === false) {
-		$content = file_get_contents('./ReplyJson/textReply.json');
-	}
-	
-	//userName會回傳為使用者名稱，如果有辦法取得的話。
-	$content = preg_replace("/userName/" , $userName , $content);
-	//keyWord會回傳為設定的關鍵字，通常就是機器人的名字。
-	$content = preg_replace("/keyWord/" , $keyWord , $content);
-		
-	$text = json_decode($content, true);
-	
-	
-	$count = 0;
-	foreach($text as $txtChack){
-		$countIn = 0;
-		$tempStr = "[";
-		foreach($txtChack['chack'] as $chack){
-			$tempStr = $tempStr . $chack ."、";
-			$count++;
-			$countIn++;
-		}
-		$tempStr = chop($tempStr,'、').']';
-		if( $count >= 4){
-			$reply = chop($reply ,'；');
-			$tempStr = "\n".$tempStr."；";
-			$count = $countIn;
-		}
-		else{
-			$tempStr = $tempStr."；";
-		}
-		$reply = $reply.$tempStr;
-	}	
-	
-	$reply = chop($reply ,'；');
-	
-
-	//抓圖片關鍵字
-	$reply = $reply."\n\n《圖片關鍵字列表》\n";
-	
-	//讀入圖片回應變數
-	$content = file_get_contents($imgsReplyUrl);
-	//如果失敗就調用預設值
-	if ($content === false) {
-		$content = file_get_contents('./ReplyJson/imgReply.json');
-	}
-	$img = json_decode($content, true);
-	
-	$count = 0;
-	foreach($img as $imgChack){
-		$countIn = 0;
-		$tempStr = "[";
-		foreach($imgChack['chack'] as $chack){
-			$tempStr = $tempStr . $chack ."、";
-			$count++;
-			$countIn++;
-		}
-		$tempStr = chop($tempStr,'、').']';
-		if( $count >= 4){
-			$reply = chop($reply ,'；');
-			$tempStr = "\n".$tempStr."；";
-			$count = $countIn;
-		}
-		else{
-			$tempStr = $tempStr."；";
-		}
-		$reply = $reply.$tempStr;
-	}		
-	$reply = chop($reply ,'；');
-	
-	return buildTextMessage($reply);	
-	
-	}
-	
-	
-	//應聲蟲功能哦
-	$input = str_replace("dvtest ","",$inputStr);
-	$finalStr = "input:\n".$input."\nstrlen:".strlen($input);
-	return buildTextMessage($finalStr);
+	return null;
 }
+
