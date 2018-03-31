00e0185fac3da93b9ffd749a04d8e3e5009987de
CHEN,Hung-Hao <36475227+howjoygroup@users.noreply.github.com>
2018-03-31 10:33:08 +0800

    Update index.php

--- LINE/index.php
+++ LINE/index.php
@@ -151,7 +151,8 @@ foreach ($bot->parseEvents() as $event) {
 			error_log("被加入聊天室");
 			$messages = new MutiMessage();
 			$replyArr = Array(
-				$messages->text("大家好，我是宏豪的秘書。\n請輸入「秘書說明」獲得使用說明～"),
+				$messages->text("大家好，我是宏豪的秘書。"),
+				$messages->text("請輸入「秘書說明」獲得使用說明～"),
 				$messages->sticker(1,2)
 			);
 			
@@ -168,7 +169,8 @@ foreach ($bot->parseEvents() as $event) {
 			error_log("被加入好友");
 			$messages = new MutiMessage();
 			$replyArr = Array(
-				$messages->text("你好哦，我是宏豪的秘書。\n請輸入「秘書說明」獲得使用說明。"),
+				$messages->text("大家好，我是宏豪的秘書。"),
+				$messages->text("請輸入「秘書說明」獲得使用說明～"),
 				$messages->sticker(4,631),
 				$messages->text("建議使用手機界面，可以更簡單的選取說明哦。")
 			);
