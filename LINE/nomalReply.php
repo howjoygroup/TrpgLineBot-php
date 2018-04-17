345adb1e777b2efe1cc0dc1b38c562da19b9cdd8
CHEN,Hung-Hao <36475227+howjoygroup@users.noreply.github.com>
2018-04-17 17:05:35 +0800

    Update nomalReply.php

--- LINE/nomalReply.php
+++ LINE/nomalReply.php
@@ -138,10 +138,14 @@ function mahjong($inputStr) {
    $textall="查詢失敗";
             $json = file_get_contents('https://spreadsheets.google.com/feeds/list/1heN-wjEuXEfqBXdN8En0WMtfR99pzg7MufiWMMi41U0/1/public/values?alt=json');
             $data = json_decode($json, true);
+		return buildTextMessage("讀json沒問題");
             foreach ($data['feed']['entry'] as $item) {
                 $keywords = explode(',', $item['gsx$遊戲id']['$t']);
+		    return buildTextMessage("第一迴圈沒問題");
    		foreach ($keywords as $keyword) {
-                	if (mb_strpos($inputStr, $keyword) !== false) {                      
+			return buildTextMessage("第二迴圈沒問題");
+                	if (mb_strpos($inputStr, $keyword) !== false) {  
+				return buildTextMessage("判斷式沒問題");
                      		$textall = "遊戲ID：".$item['gsx$遊戲id']['$t'].
                     		"\n本名：".$item['gsx$本名']['$t'].
                     		"\n歐付寶ID：".$item['gsx$歐付寶id']['$t'].
