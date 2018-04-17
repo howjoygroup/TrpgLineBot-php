461f975d578e4c63026028bd3309aeb68c203723
CHEN,Hung-Hao <36475227+howjoygroup@users.noreply.github.com>
2018-04-17 17:48:58 +0800

    Update nomalReply.php

--- LINE/nomalReply.php
+++ LINE/nomalReply.php
@@ -136,7 +136,7 @@ function SendImg($inputStr) {
 //麻將玩家查詢系統
 function mahjong($inputStr) {
       $textall="查詢失敗";
-            $json = file_get_contents('https://spreadsheets.google.com/feeds/list/1heN-wjEuXEfqBXdN8En0WMtfR99pzg7MufiWMMi41U0/1/public/values?alt=json');
+            $json = file_get_contents('https://spreadsheets.google.com/feeds/list/1XCyhDXNJNP-tT3PG8VCe82y79fNoVigo-w8QF_SiyTU/1/public/values?alt=json');
             $data = json_decode($json, true);
             foreach ($data['feed']['entry'] as $item) {
                 $keywords = explode(',', $item['gsx$遊戲id']['$t']);
