646c9010bb8456ef89c6a9252d1b6a8a1e76f798
CHEN,Hung-Hao <36475227+howjoygroup@users.noreply.github.com>
2018-04-19 02:45:19 +0800

    Update nomalReply.php

--- LINE/nomalReply.php
+++ LINE/nomalReply.php
@@ -159,6 +159,7 @@ function mahjong2($inputStr) {
             foreach ($data2['feed']['entry'] as $item2) {
                 $keywords2 = explode(',', $item2['gsx$遊戲id']['$t']);
      		 foreach ($keywords2 as $keyword2) {
+			 return buildTextMessage($keyword2);
 			$keyword2 = "m"+$keyword2; 
                  	if ($inputStr == $keyword2) {  
                        		$textall2 = "遊戲ID：".$item2['gsx$遊戲id']['$t'].
