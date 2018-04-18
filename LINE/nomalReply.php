d60a8f2503998b626ffc183b10d9095998237723
CHEN,Hung-Hao <36475227+howjoygroup@users.noreply.github.com>
2018-04-19 01:51:20 +0800

    Update nomalReply.php

--- LINE/nomalReply.php
+++ LINE/nomalReply.php
@@ -147,9 +147,6 @@ function mahjong($inputStr) {
                       		"\n歐付寶ID：".$item['gsx$歐付寶id']['$t'].
                        		"\n代理：".$item['gsx$代理']['$t'];
                      	}
-			else{
-				return mahjong2($inputStr);
-			}
                 }
             }
             return buildTextMessage($textall);
